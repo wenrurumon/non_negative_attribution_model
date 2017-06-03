21faa68722b3e2f1b5e8bbdd06d1726ffad6f08b
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-03 15:43:05 -0500

    Update data_process.R

--- comparison_test/data_process.R
+++ comparison_test/data_process.R
@@ -4,7 +4,9 @@ setwd("/home/zhu/deconv/data")
 load('loaddata.rda')
 
 pca1 <- function(X,prop=1){
+  X[is.na(X)] <- 0
   X <- scale(X)[,]
+  X[is.na(X)] <- 0
   m = nrow(X)
   n = ncol(X)
   Xeigen <- svd(X)
@@ -12,6 +14,18 @@ pca1 <- function(X,prop=1){
   value <- cumsum(value/sum(value))
   score <- X %*% Xeigen$v
   score[,1]
+}
+
+pca <- function(X,prop=1){
+  X <- scale(X)[,]
+  m = nrow(X)
+  n = ncol(X)
+  Xeigen <- svd(X)
+  value <- (Xeigen$d)^2/m
+  value <- cumsum(value/sum(value))
+  score <- X %*% Xeigen$v
+  score2 <- score[,1:which(value>=prop)[1],drop=F]
+  list(score=score,score2=score2,value=value,mat=Xeigen$v)
 }
 
 #adnidata
@@ -26,14 +40,30 @@ pca1 <- function(X,prop=1){
   g <- unique(colnames(A))
   g <- g[g%in%gene_sel]
   A <- A[,colnames(A)%in%g]
-  Asplit <- sapply(g,function(gi){
+  Asplit <- sapply(gene_sel,function(gi){
     temp <- A[,colnames(A)==gi,drop=F]
     pca1(temp)
   })
+rownames(Asplit) <- rownames(A)
 adnidata$A2 <- Asplit
 
 #rushdata
   A <- rushdata$A
-  g <- unique(rushdata$gene)
-  g <- g[g%in%gene_sel]
-  A <- apply(A,2,as.numeric)
+  A <- A[,colnames(A)%in%gene_sel]
+  A <- scale(apply(A,2,as.numeric))
+  rownames(A) <- rownames(rushdata$A)
+  i <- 0
+  Asplit <- sapply(gene_sel,function(gi){
+    print((i <<- i+1)/length(gene_sel))
+    temp <- pca1(A[,colnames(A)==gi,drop=F])
+  })
+  rownames(Asplit) <- rownames(A)
+  rushdata$A2 <- Asplit
+
+#reference
+  X <- apply(refdata$X,2,as.numeric)
+  dimnames(X) <- dimnames(refdata$X)
+  Xsplit <- lapply(unique(refdata$cell),function(ci){
+    print(ci)
+    pca(X[,refdata$cell==ci,drop=F],prop=0.6)
+  })
