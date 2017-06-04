b20f5ad5912d28e8e953db7f0a66ad2888fd5304
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-04 16:00:41 -0500

    Create association_test.R

---
+++ comparison_test/association_test.R
@@ -0,0 +1,24 @@
+
+rm(list=ls())
+setwd("/home/zhu/deconv")
+load('data/rlt4compare2.rda')
+
+asso <- function(d,y,pen=0){
+  y <- (y-pen) * ((y-pen)>0)
+  apply(y,2,function(x){
+    t.test(x~d)$p.value
+  })
+}
+
+assob <- function(d,ys,yn){
+  rlt = list()
+  rlt[[1]] <- asso(d,ys)
+  rlt[[2]] <- asso(d,yn)
+  for(i in 3:5){
+    rlt[[i]] <- asso(d,yn,pen=(0.05)*(i-2))
+  }
+  do.call(cbind,rlt)
+}
+
+testr <- assob(dr,Yrn,Yrs)
+testa <- assob(da,Yan,Yas)
