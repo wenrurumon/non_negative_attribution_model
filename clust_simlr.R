e1cd183b737e2afbfd2abe96bcbff5ff1d1d19e4
進撃の雯雯老師 <askahu6@gmail.com>
2017-10-05 06:40:47 +0800

    Update clust_simlr.R

--- clust_simlr.R
+++ clust_simlr.R
@@ -7,9 +7,10 @@ library(igraph)
 #setwd('e:\\uthealth\\deconv')
 setwd('/zfsauton/project/highmark/aska/sc2')
 load("data4simlr.rda")
-browseVignettes("SIMLR")
+#browseVignettes("SIMLR")
 
 length(unique(data$clust$celltype))
-test <- SIMLR(X = data$ref, c = 17, cores.ratio = 0)
+system.time(test <- SIMLR(X = data$ref, c = 17, cores.ratio = 0))
+save(test,file='test_rlt.rda')
 
 table(test$y$cluster,data$clust$celltype[1:length(test$y$cluster)])
