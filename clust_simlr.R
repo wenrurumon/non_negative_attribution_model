b13dfa8abb1a427d0c870b249200bb85783face6
進撃の雯雯老師 <askahu6@gmail.com>
2017-10-05 06:33:54 +0800

    Create clust_simlr.R

---
+++ clust_simlr.R
@@ -0,0 +1,15 @@
+
+rm(list=ls())
+library(SIMLR)
+library(data.table)
+library(igraph)
+
+#setwd('e:\\uthealth\\deconv')
+setwd('/zfsauton/project/highmark/aska/sc2')
+load("data4simlr.rda")
+browseVignettes("SIMLR")
+
+length(unique(data$clust$celltype))
+test <- SIMLR(X = data$ref, c = 17, cores.ratio = 0)
+
+table(test$y$cluster,data$clust$celltype[1:length(test$y$cluster)])
